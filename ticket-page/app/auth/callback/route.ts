0f10570f6098d6f9391f00686debdb24985814b8
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-04-17 19:08:49 +0100

    Update route.ts

--- ticket-page/app/auth/callback/route.ts
+++ ticket-page/app/auth/callback/route.ts
@@ -7,31 +7,26 @@ export async function GET(request: NextRequest) {
   const code = searchParams.get("code");
   const redirect = searchParams.get("redirect") || "/";
 
-  if (!code) {
-    return NextResponse.redirect(`${origin}${redirect}`);
+  if (code) {
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          },
+        },
+      }
+    );
+    await supabase.auth.exchangeCodeForSession(code);
   }
-
-  const cookieStore = cookies();
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options)
-          );
-        },
-      },
-    }
-  );
-
-  await supabase.auth.exchangeCodeForSession(code);
 
   return NextResponse.redirect(`${origin}${redirect}`);
 }
-
